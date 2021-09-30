cd6885b87a8069b2bac8f52efac1d24773d30367
Milan Cop <milan.cop@infodyn.eu>
2021-09-30 11:25:23 +0200

    fixed head icon

--- Report-Asapp.Android/Resources/Resource.designer.cs
+++ Report-Asapp.Android/Resources/Resource.designer.cs
@@ -12203,13 +12203,16 @@ namespace Report_Asapp.Droid
 			public const int rassap_bckg = 2131165328;
 			
 			// aapt resource value: 0x7F070091
-			public const int test_custom_background = 2131165329;
+			public const int rassap_bw = 2131165329;
 			
 			// aapt resource value: 0x7F070092
-			public const int tooltip_frame_dark = 2131165330;
+			public const int test_custom_background = 2131165330;
 			
 			// aapt resource value: 0x7F070093
-			public const int tooltip_frame_light = 2131165331;
+			public const int tooltip_frame_dark = 2131165331;
+			
+			// aapt resource value: 0x7F070094
+			public const int tooltip_frame_light = 2131165332;
 			
 			static Drawable()
 			{
