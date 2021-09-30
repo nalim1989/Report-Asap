e6d2c4547f204c0b47712e806115072e625f69bd
Milan Cop <milan.cop@infodyn.eu>
2021-09-30 14:12:47 +0200

    add Welcome Binding

--- Report-Asapp/WelcomePage.xaml.cs
+++ Report-Asapp/WelcomePage.xaml.cs
@@ -4,15 +4,19 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ReportAsapp.ViewModel;
 using Xamarin.Forms;
 
 namespace Report_Asapp
 {
     public partial class WelcomePage : ContentPage
     {
+        private WelcomeViewModel welcomeViewModel;
         public WelcomePage()
         {
+            welcomeViewModel = new WelcomeViewModel();
             InitializeComponent();
+            BindingContext = welcomeViewModel;
         }
     }
 }
